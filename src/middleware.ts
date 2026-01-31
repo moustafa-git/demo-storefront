431755d0cb92a2e5c43cf52bbf166ac87b49d2cc
moustafa-git <mostax266@gmail.com>
2026-01-31 18:09:31 +0200

    frame2

--- src/middleware.ts
+++ src/middleware.ts
@@ -129,13 +129,16 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
-  // if one of the country codes is in the url and the cache id is not set, set the cache id and redirect
+  // if one of the country codes is in the url and the cache id is not set, set the cache id and continue
   if (urlHasCountryCode && !cacheIdCookie) {
-    response.cookies.set("_medusa_cache_id", cacheId, {
+    const nextResponse = NextResponse.next()
+    nextResponse.cookies.set("_medusa_cache_id", cacheId, {
       maxAge: 60 * 60 * 24,
+      sameSite: "none",
+      secure: true,
     })
 
-    return response
+    return nextResponse
   }
 
   // check if the url is a static asset
